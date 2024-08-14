b191a8e17ced80e3b23e031fcbcae049fb95e6a8
QJPROMPT <QinN@prompteng.com.au>
2024-08-14 21:23:35 +0800

    add new environment variable for db.js

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
-const Pool = require("pg").Pool;
+const { Pool } = require('pg');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 const pool = new Pool({
-  user: "qinjieng",
-  host: "localhost",
-  port: 5432,
-  database: "mangacollections",
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
 module.exports = pool;
